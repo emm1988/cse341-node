1e0dea7e4631ce27286c72d5809d270e8a50b1eb
emm1988 <emmanueljepsal21@gmail.com>
2026-09-09 16:06:32 -0300

    correcting welcome mssg

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my CSE341');
+    res.send('Welcome to CSE341');
 });
 
 app
