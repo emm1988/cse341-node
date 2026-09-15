4a285bc6f2dba89713df4805ba2ee15cb5ac1f25
emm1988 <emmanueljepsal21@gmail.com>
2026-09-15 19:37:35 -0300

    hosting correction

--- backend/swagger.js
+++ backend/swagger.js
@@ -1,8 +1,8 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 // Render and local hosting for Swagger documentation
-const isProduction = process.env.NODE_ENV === 'production';
-const hostUrl = isProduction ? 'cse341-node-tpx2.onrender.com' : 'localhost:8080';
+const isProduction = process.env.NODE_ENV === 'production'|| process.argv.includes('--prod');
+const hostUrl = isProduction ? '//onrender.com' : 'localhost:8080';
 
 const doc = {
   info: {
